9101096ae2c645f3118463ab522f1ecd0895b595
zola26 <148457096+zola26@users.noreply.github.com>
2024-08-20 09:25:44 +0300

    Row

---
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Row from "../Row/Row"
+import requests from '../../../utiles/requests'
+const RowList = () => {
+  return (
+    <>
+      <Row
+       title="NETFLIX ORIGINAL"
+       fetchUrl={requests.fetchNetflixOriginals}
+       isLargeRow={true}
+      />
+      <Row
+      title="Tranding Now"
+      fetchUrl={requests.fetchTrending}
+      />
+      <Row 
+       title="Top Rated" 
+       fetchUrl={requests.fetchTopRatedMovies}
+       />
+      <Row 
+      title="Action Movies" 
+      fetchUrl={requests.fetchActionMovies} 
+      />
+      <Row 
+      title="Comedy Movies" 
+      fetchUrl={requests.fetchComedyMovies} 
+      />
+      <Row 
+      title="Horror Movies" 
+      fetchUrl={requests.fetchHorrorMovies} 
+      />
+      <Row 
+      title="Romance Movies" 
+      fetchUrl={requests.fetchRomanceMovies} />
+      <Row 
+      title="TV Shows" 
+      fetchUrl={requests.fetchTvShow} />
+      <Row 
+      title="Documentaries" 
+      fetchUrl={requests.fetchDocumentaries} />
+    </>
+  )
+}
+
+export default RowList
--- netflix-clone/src/pages/Main/Main.js
+++ netflix-clone/src/pages/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowList/RowList'
 
 const Main = () => {
   return (
     <>
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
